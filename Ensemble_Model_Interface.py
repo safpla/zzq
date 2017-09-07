51e1159b34af6a75e15b470491fff6d4b4bfbd24
safpla <haowen.will.xu@gmail.com>
2017-09-07 16:02:55 +0800

    Ensemble interface

---
+++ Ensemble_Model_Interface.py
@@ -0,0 +1,39 @@
+from abc import ABCMeta, abstractmethod
+
+class Ensemble_Model_Interface(object):
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def txt2ind(self, txt):
+        """
+        You need to provide this function if you didn't use Li Shen's preprocessing, or you can just pass
+        :param txt: list of strings
+        :return: list of strings' index, it will be used as the input of "predict" function
+        """
+        return
+
+    @abstractmethod
+    def predict(self, sess, sent):
+        """
+        Do prediction
+        :param sess: tf.Session()
+        :param sent: input sentences
+                     sent is a list of sentences
+                     each sentence is a list of index, unpadded
+                     or you can use the return of "txt2ind" if provided
+
+        :return: list of labels
+                 encoding mode:
+                 [1,0,0,0,0,0,0,0]: 非影视作品
+                 [0,1,0,0,0,0,0,0]: 电视剧
+                 [0,0,1,0,0,0,0,0]: 电影
+                 [0,0,0,1,0,0,0,0]: 综艺节目
+                 [0,0,0,0,1,0,0,0]: 纪录片
+                 [0,0,0,0,0,1,0,0]: 动画片
+                 [0,0,0,0,0,0,1,0]: 综艺节目
+                 [0,0,0,0,0,0,0,1]: 电视节目
+        """
+        return
